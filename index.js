68c79cbd636e1ee1a4b0d2098f933bd13ceb6963
Emmalee <emilee.wimmer@gmail.com>
2022-09-13 09:28:22 -0600

    added console inputs and req packages

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [{
+  type: 'input',
+  message: 'What is your Github username?',
+  name: 'github'
+},
+{
+  type: 'input',
+  message: 'What is your email?',
+  name: ' email'
+},
+
+  {
   type: 'input',
   message: 'what is the title of your project?',
   name: 'title'
@@ -11,10 +23,31 @@ const questions = [{
   message: 'give a breif description of your project',
   name:'description'
 },
-{}
-
-
-
+{
+  type: 'input',
+  message: 'how do you install this application?',
+  name: 'install'
+},
+{
+  type: 'input',
+  message: 'how should this application be used?',
+  name: 'usage'
+},
+{
+  type: 'input',
+  message: 'How can others contribute to this project?',
+  name: 'contribution'
+},
+{
+  type:'input',
+  message: 'how can others test this application?',
+  name: 'tests'
+},
+{
+  type: 'list',
+  message: 'which license should be shown on this project?',
+  name: ' license'
+}
 ];
 
 // TODO: Create a function to write README file
