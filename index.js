508734bf904b8ffc331c340874c1e522d0b7ac6f
Emmalee <emilee.wimmer@gmail.com>
2022-09-13 09:14:09 -0600

    adding base code

---
+++ index.js
@@ -0,0 +1,27 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [{
+  type: 'input',
+  message: 'what is the title of your project?',
+  name: 'title'
+},
+{
+  type: 'input',
+  message: 'give a breif description of your project',
+  name:'description'
+},
+{}
+
+
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
